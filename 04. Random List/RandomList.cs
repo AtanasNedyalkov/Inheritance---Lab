837498755f238befd397c715e93ad78f6c5a8a9a
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-07-27 14:31:01 +0300

    Add project files.

---
+++ 04. Random List/RandomList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList:List<string>
+    {
+        private Random rnd { get; set; }
+        public RandomList(Random rnd)
+        {
+            this.rnd = rnd;
+        }
+        public RandomList()
+        {
+        }
+        public string RemoveRandomElement()
+        {
+            string str = string.Empty;
+            int index = rnd.Next(0, this.Count);
+            str = this[index];
+            this.RemoveAt(index);
+            return str;
+        }
+        public int RandomInteger()
+        {
+            return this.rnd.Next(0,int.MaxValue);
+        }
+        public int RandomString() => this.RandomInteger();
+    }
+}
---
+++ 04. Random List/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rl = new RandomList();
+            rl.Add("ggg");
+            rl.Add("ttt");
+            rl.RandomString();
+            rl.RandomString();
+          
+
+            rl.RemoveRandomElement();
+
+            foreach (var element in rl)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
